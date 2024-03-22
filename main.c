8ff9b3efb5728c293d19df2e57ee06d2427f348e
AbubakerJassim <163153320+AbubakerJassim@users.noreply.github.com>
2024-03-22 11:38:10 -0400

    I have gotten the bird drawn and moving

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+//50 coloumns, and 38 rows
 const unsigned short Bird[1900] = {
 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF,   // 0x0010 (16) pixels
 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF,   // 0x0020 (32) pixels
@@ -119,4 +123,87 @@ const unsigned short Bird[1900] = {
 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF,   // 0x0760 (1888) pixels
 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF
 };
+	
+volatile int pixel_buffer_start; // global variable
+short int Buffer1[240][512]; // 240 rows, 512 (320 + padding) columns
+short int Buffer2[240][512];
 
+
+void wait_for_vsync () {
+	volatile int * pixel_ctrl_ptr = (int *)0xFF203020;
+	int status;
+	*pixel_ctrl_ptr = 1;
+	status = *(pixel_ctrl_ptr + 3);
+	while((status&1)!=0) {
+		status = *(pixel_ctrl_ptr + 3);
+	}
+}
+
+void plot_pixel(int x, int y, short int line_color) {
+    volatile short int *one_pixel_address;
+	one_pixel_address = pixel_buffer_start + (y << 10) + (x << 1);
+	*one_pixel_address = line_color;
+}
+
+void clear_screen() {
+	for(int x = 0; x<320; x++) {
+		for(int y = 0; y < 240; y++) {
+			plot_pixel(x,y,0x0);
+		}
+	}
+}
+
+void draw_bird(int x, int y) {
+	int number = 0;
+	for(int i = 0; i<1900; i++) {
+		if(i%50==0) {
+			y = y+1;
+			number = 0;
+		}
+		
+		plot_pixel(x+number,y,Bird[i]);
+		number++;
+	}
+}
+
+int main(void)
+{
+    volatile int * pixel_ctrl_ptr = (int *)0xFF203020;
+    // declare other variables(not shown)
+    // initialize location and direction of rectangles(not shown)
+	int x = 135;
+	int y = 101;
+
+    /* set front pixel buffer to Buffer 1 */
+    *(pixel_ctrl_ptr + 1) = (int) &Buffer1; // first store the address in the  back buffer
+    /* now, swap the front/back buffers, to set the front buffer location */
+    wait_for_vsync();
+    /* initialize a pointer to the pixel buffer, used by drawing functions */
+    pixel_buffer_start = *pixel_ctrl_ptr;
+    clear_screen(); // pixel_buffer_start points to the pixel buffer
+
+    /* set back pixel buffer to Buffer 2 */
+    *(pixel_ctrl_ptr + 1) = (int) &Buffer2;
+    pixel_buffer_start = *(pixel_ctrl_ptr + 1); // we draw on the back buffer
+    clear_screen(); // pixel_buffer_start points to the pixel buffer
+
+    while (1)
+    {
+        /* Erase any boxes and lines that were drawn in the last iteration */
+
+
+        // code for drawing the boxes and lines (not shown)
+        // code for updating the locations of boxes (not shown)
+		clear_screen();
+		draw_bird(x,y);
+        wait_for_vsync(); // swap front and back buffers on VGA vertical sync
+        pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer
+		x = x+1;
+		y = y+1;
+		if(((x+50)==319)||((y+38)==239)) {
+			break;
+		}
+	}
+}
+
+	
