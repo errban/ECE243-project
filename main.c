4c8f2ae9a8b04a9418bf192c587c58161da6f223
AbubakerJassim <163153320+AbubakerJassim@users.noreply.github.com>
2024-03-25 11:56:27 -0400

    progress in setting up interrupts

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+
+#ifndef __NIOS2_CTRL_REG_MACROS__
+#define __NIOS2_CTRL_REG_MACROS__
+/*****************************************************************************/
+/* Macros for accessing the control registers. */
+/*****************************************************************************/
+#define NIOS2_READ_STATUS(dest) \
+do { dest = __builtin_rdctl(0); } while (0)
+#define NIOS2_WRITE_STATUS(src) \
+do { __builtin_wrctl(0, src); } while (0)
+#define NIOS2_READ_ESTATUS(dest) \
+do { dest = __builtin_rdctl(1); } while (0)
+#define NIOS2_READ_BSTATUS(dest) \
+do { dest = __builtin_rdctl(2); } while (0)
+#define NIOS2_READ_IENABLE(dest) \
+do { dest = __builtin_rdctl(3); } while (0)
+#define NIOS2_WRITE_IENABLE(src) \
+do { __builtin_wrctl(3, src); } while (0)
+#define NIOS2_READ_IPENDING(dest) \
+do { dest = __builtin_rdctl(4); } while (0)
+#define NIOS2_READ_CPUID(dest) \
+do { dest = __builtin_rdctl(5); } while (0)
+#endif
+
+volatile int pixel_buffer_start; // global variable
+short int Buffer1[240][512]; // 240 rows, 512 (320 + padding) columns
+short int Buffer2[240][512];
+
 //50 coloumns, and 38 rows
 const unsigned short Bird[1900] = {
 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF,   // 0x0010 (16) pixels
@@ -1327,13 +1355,118 @@ const unsigned short Circle[19200] = {
 0xEF5D, 0xEF5D, 0xEF5D, 0xEF5D, 0xEF5D, 0xEF5D, 0xEF5D, 0xEF5D, 0xEF5D, 0xEF5D, 0xEF5D, 0xEF5D, 0xEF5D, 0xEF5D, 0xEF5D, 0xEF5D,   // 0x4B00 (19200) pixels
 };
 
+bool clicked = true;
+void interrupt_handler(void);
+void interrupt_handler(void) {
+	int PS2_data, RVALID;
+	char byte1 = 0, byte2 = 0, byte3 = 0;
+	byte1 = byte2;
+	byte2 = byte3;
+	byte3 = PS2_data & 0xFF;
+}
 
+void the_reset(void) __attribute__((section(".reset")));
+void the_reset(void)
+/*******************************************************************************
+* Reset code. By giving the code a section attribute with the name ".reset" we
+* allow the linker program to locate this code at the proper reset vector
+* address. This code just calls the main program.
+******************************************************************************/
+{
+	asm(".set noat"); /* Instruct the assembler NOT to use reg at (r1) as
+	* a temp register for performing optimizations */
+	asm(".set nobreak"); /* Suppresses a warning message that says that
+	* some debuggers corrupt regs bt (r25) and ba
+	* (r30)
+	*/
+	asm("movia r2, main"); // Call the C language main program
+	asm("jmp r2");
+}
 
-	
-volatile int pixel_buffer_start; // global variable
-short int Buffer1[240][512]; // 240 rows, 512 (320 + padding) columns
-short int Buffer2[240][512];
-
+void the_exception(void) __attribute__((section(".exceptions")));
+void the_exception(void)
+/*******************************************************************************
+* Exceptions code. By giving the code a section attribute with the name
+* ".exceptions" we allow the linker program to locate this code at the proper
+* exceptions vector address.
+* This code calls the interrupt handler and later returns from the exception.
+******************************************************************************/
+{
+	asm("subi sp, sp, 128");
+	asm("stw et, 96(sp)");
+	asm("rdctl et, ctl4");
+	asm("beq et, r0, SKIP_EA_DEC"); // Interrupt is not external
+	asm("subi ea, ea, 4"); /* Must decrement ea by one instruction
+	* for external interupts, so that the
+	* interrupted instruction will be run */
+	asm("SKIP_EA_DEC:");
+	asm("stw r1, 4(sp)"); // Save all registers
+	asm("stw r2, 8(sp)");
+	asm("stw r3, 12(sp)");
+	asm("stw r4, 16(sp)");
+	asm("stw r5, 20(sp)");
+	asm("stw r6, 24(sp)");
+	asm("stw r7, 28(sp)");
+	asm("stw r8, 32(sp)");
+	asm("stw r9, 36(sp)");
+	asm("stw r10, 40(sp)");
+	asm("stw r11, 44(sp)");
+	asm("stw r12, 48(sp)");
+	asm("stw r13, 52(sp)");
+	asm("stw r14, 56(sp)");
+	asm("stw r15, 60(sp)");
+	asm("stw r16, 64(sp)");
+	asm("stw r17, 68(sp)");
+	asm("stw r18, 72(sp)");
+	asm("stw r19, 76(sp)");
+	asm("stw r20, 80(sp)");
+	asm("stw r21, 84(sp)");
+	asm("stw r22, 88(sp)");
+	asm("stw r23, 92(sp)");
+	asm("stw r25, 100(sp)"); // r25 = bt (skip r24 = et, because it is saved
+	// above)
+	asm("stw r26, 104(sp)"); // r26 = gp
+	// skip r27 because it is sp, and there is no point in saving this
+	asm("stw r28, 112(sp)"); // r28 = fp
+	asm("stw r29, 116(sp)"); // r29 = ea
+	asm("stw r30, 120(sp)"); // r30 = ba
+	asm("stw r31, 124(sp)"); // r31 = ra
+	asm("addi fp, sp, 128");
+	asm("call interrupt_handler"); // Call the C language interrupt handler
+	asm("ldw r1, 4(sp)"); // Restore all registers
+	asm("ldw r2, 8(sp)");
+	asm("ldw r3, 12(sp)");
+	asm("ldw r4, 16(sp)");
+	asm("ldw r5, 20(sp)");
+	asm("ldw r6, 24(sp)");
+	asm("ldw r7, 28(sp)");
+	asm("ldw r8, 32(sp)");
+	asm("ldw r9, 36(sp)");
+	asm("ldw r10, 40(sp)");
+	asm("ldw r11, 44(sp)");
+	asm("ldw r12, 48(sp)");
+	asm("ldw r13, 52(sp)");
+	asm("ldw r14, 56(sp)");
+	asm("ldw r15, 60(sp)");
+	asm("ldw r16, 64(sp)");
+	asm("ldw r17, 68(sp)");
+	asm("ldw r18, 72(sp)");
+	asm("ldw r19, 76(sp)");
+	asm("ldw r20, 80(sp)");
+	asm("ldw r21, 84(sp)");
+	asm("ldw r22, 88(sp)");
+	asm("ldw r23, 92(sp)");
+	asm("ldw r24, 96(sp)");
+	asm("ldw r25, 100(sp)"); // r25 = bt
+	asm("ldw r26, 104(sp)"); // r26 = gp
+	// skip r27 because it is sp, and we did not save this on the stack
+	asm("ldw r28, 112(sp)"); // r28 = fp
+	asm("ldw r29, 116(sp)"); // r29 = ea
+	asm("ldw r30, 120(sp)"); // r30 = ba
+	asm("ldw r31, 124(sp)"); // r31 = ra
+	asm("addi sp, sp, 128");
+	asm("eret");
+}
 
 void wait_for_vsync () {
 	volatile int * pixel_ctrl_ptr = (int *)0xFF203020;
@@ -1409,6 +1542,11 @@ void draw_reverse_bird(int x, int y) {
 int main(void)
 {
     volatile int * pixel_ctrl_ptr = (int *)0xFF203020;
+	volatile int * PS2_ptr = (int *) 0xFF200100;
+	*(PS2_ptr+1) = 1;
+	
+	NIOS2_WRITE_IENABLE(0x40);
+	NIOS2_WRITE_STATUS(1);
     // declare other variables(not shown)
     // initialize location and direction of rectangles(not shown)
 	int x = 135;
@@ -1468,3 +1606,5 @@ int main(void)
 		}
 	}
 }
+
+	
